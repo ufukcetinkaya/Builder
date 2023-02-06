a5031a0deb45f1d6e8921392ecdba7e87cdc527b
ufuk.cetinkaya <ufuk.cetinkaya@icterra.com>
2023-02-06 16:31:29 +0300

    Method creation is added

--- Builder/Builder.cpp
+++ Builder/Builder.cpp
@@ -1,81 +1,34 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <ostream>
-#include <iostream>
 #include <sstream>
 using namespace std;
 
-//struct tag
-//{
-//    std::string name;
-//    std::string text;
-//    std::vector<tag> children;
-//    std::vector<std::pair<std::string, std::string>> attributes;
-//
-//    friend std::ostream& operator<<(std::ostream& os, const tag& tag)
-//    {
-//        os << "<" << tag.name;
-//
-//        for (const auto& att : tag.attributes)
-//            os << " " << att.first << "=\"" << att.second << "\"";
-//
-//        if (tag.children.empty() && tag.text.length() == 0)
-//        {
-//            os << "/>" << std::endl;
-//        }
-//        else
-//        {
-//            os << ">" << std::endl;
-//
-//            if (tag.text.length())
-//                os << tag.text << std::endl;
-//
-//            for (const auto& child : tag.children)
-//                os << child;
-//
-//            os << "</" << tag.name << ">" << std::endl;
-//        }
-//
-//        return os;
-//    }
-//protected:
-//
-//    tag(std::string name, std::string text)
-//        : name{std::move(name)},
-//        text{std::move(text)}
-//    {
-//    }
-//
-//
-//    tag(std::string name, std::vector<tag> children)
-//        : name{std::move(name)},
-//        children{std::move(children)}
-//    {
-//    }
-//};
-//
-//struct p : tag
-//{
-//    explicit p(const std::string& text)
-//        : tag{ "p", text }
-//    {
-//    }
-//
-//    p(std::initializer_list<tag> children)
-//        : tag("p", children)
-//    {
-//    }
-//
-//};
-//
-//struct img : tag
-//{
-//    explicit img(const std::string& url)
-//        : tag{ "img", "" }
-//    {
-//        attributes.emplace_back("src", url);
-//    }
-//};
+template <typename Range, typename Value = typename Range::value_type>
+    string join(Range const& elements, const char *const delimiter)
+{
+        ostringstream os;
+        auto b = begin(elements), e = end(elements);
+
+        if (b != e) 
+        {
+            copy(b, prev(e), std::ostream_iterator<Value>(os, delimiter));
+            b = prev(e);
+        }
+        if (b != e) 
+        {
+            os << *b;
+        }
+
+        return os.str();
+}
+
+enum file_type
+{
+	file_cpp = 0,
+    file_h
+};
 
 struct arguments
 {
@@ -98,17 +51,53 @@ struct field
     }
 };
 
+struct method
+{
+	string name, type;
+    vector<arguments> args;
+
+    method(string name, string type, vector<arguments> arguments)
+	    : name{std::move(name)},
+	      type{std::move(type)},
+          args{std::move(arguments)}
+    {
+    }
+
+    friend ostream& operator<<(ostream& os, const method& obj)
+    {
+        vector<string> strings;
+    	os << "\n\t" << obj.type << " " << obj.name << "(";
+        for (const auto& ar : obj.args)
+        {
+            string s = ar.type + " " + ar.name;
+            strings.emplace_back(s);
+        }
+        os << join(strings, ", ");
+        os << ")";
+        os << ";\n";
+		//os << "\t{\n\t}";
+
+        return os;
+    }
+};
+
 struct Class
 {
     string name;
     vector<field> fields;
+    vector<method> methods;
 
     friend ostream& operator<<(ostream& os, const Class& obj)
     {
         os << "class " << obj.name << "\n{\n";
         for (auto&& field : obj.fields)
         {
-            os << "  " << field << "\n";
+            os << "\t" << field << "\n";
+        }
+
+        for (auto&& method : obj.methods)
+        {
+	        os << "\t" << method << "\n";
         }
         return os << "};\n";
     }
@@ -123,15 +112,10 @@ public:
         the_class_.name = class_name;
     }
 
-    code_builder& add_field(const string& name, const string& type)
+    code_builder& add_field(const string& name, const string& type, const vector<arguments>& args)
     {
         the_class_.fields.emplace_back(name, type);
-        return *this;
-    }
-
-    code_builder& add_method(const string& name, const string& type)
-    {
-        the_class_.fields.emplace_back(name, type);
+        the_class_.methods.emplace_back(name, type, args);
         return *this;
     }
 
@@ -144,11 +128,25 @@ public:
 
 int main()
 {
+    const arguments args = {"test", "string"};
+    vector<arguments> ar;
+    ar.emplace_back(args);
+    ar.emplace_back(args);
 	const auto cb = code_builder{ "Person" }
-	                .add_field("name", "string")
-	                .add_field("age", "int");
+	                .add_field("name", "string", ar)
+	                .add_field("age", "int", ar);
     ostringstream oss;
     oss << cb;
     std::cout << oss.str() << std::endl;
+
+    /*string elements[] = { "aap", "noot", "mies" };
+
+    typedef vector<string> strings;
+
+    
+    ostringstream oss;
+    oss << join(strings(elements, elements + 3), ", ");;
+    std::cout << oss.str() << std::endl;*/
+    
     return 0;
 }
